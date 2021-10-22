7b18d667a82169a2d8a732c27a6faf8401809e8c
gonzaloperezdelacampa <79965499+gonzaloperezdelacampa@users.noreply.github.com>
2021-10-22 13:00:12 +0000

    Add files via upload

---
+++ contacto/aviso_legal.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<meta http-equiv="Content-Language" content="es" />
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+<link rel="stylesheet" type="text/css" href="../css/mar.css">
+
+<title>Aviso Legal :: María del Mar Camacho Santiago</title>
+</head>
+<body id="aviso_legal">
+
+<div id="aviso_txt">
+<p class="titulo_aviso">Aviso Legal</p>
+<br>
+   <p>Todos los datos facilitados a través de formularios electrónicos y/o mediante correo electrónico serán tratados con estricta confidencialidad de acuerdo con la Ley orgánica 15/1.999, de 13 de diciembre, de Protección de Datos de Carácter Personal, y le informamos que tiene usted derecho de acceso al fichero, rectificación y cancelación de sus datos, pudiendo ejercitar tales derechos enviando por escrito una solicitud a la dirección calle Burgos 13, Bajo C, 28039 Madrid, España.</p><br><br>
+
+	<p>Los datos solicitados son los estrictamente necesarios para la correcta identificación del remitente, llevar a cabo tareas básicas de administración y gestión. María del Mar Camacho Santiago insiste en su compromiso de uso responsable y confidencial de los datos, garantizando que los datos de los usuarios se tratarán de acuerdo con las exigencias legales, y que tan sólo serán utilizados por María del Mar Camacho Santiago.</p><br><br>
+
+	<p>Los datos recabados son los adecuados, pertinentes y no excesivos en relación con el ámbito y finalidades descritos. El usuario responderá, en cualquier caso, de la veracidad de los datos facilitados, reservándose María del Mar Camacho Santiago el derecho a excluir de los servicios registrados a todo usuario que haya facilitado datos falsos, sin perjuicio de las demás acciones que procedan en Derecho.</p><br><br>
+
+	<p>Mediante el envío de cualquier formulario existente en estas páginas Web o mediante el envío de un mensaje de correo electrénico, el remitente presta su consentimiento expreso al tratamiento de los datos incluidos en el mismo. En cualquier caso, los datos facilitados no serán objeto de cesión a terceros.</p>
+</div>
+</body>
+</html>
